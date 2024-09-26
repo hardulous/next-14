cc48b0f083acfd4667e24c946895935223fb62dd
hardulous <bishtaman120@gmail.com>
2024-09-26 23:29:40 +0530

    started routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
+
+// This page component inside about folder maps to "localhost:3000/about"
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
+
+
+// This page component inside profile folder maps to "localhost:3000/profile"
