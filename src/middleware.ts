50320571a1b856f175fa8138c00681710dc019a4
hardulous <bishtaman120@gmail.com>
2024-10-07 23:37:38 +0530

    started rendering

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,38 @@ import { NextResponse, type NextRequest } from "next/server";
 
 // A function which will execute when
 export function middleware(req: NextRequest) {
+  const response = NextResponse.next(); // To execute next middleware in the sequence
+
+  // Here working with cookies
+  const themePrefernce = req.cookies.get("theme");
+  if (!themePrefernce) {
+    response.cookies.set("theme", "dark");
+  }
+
+  // Working with headers
+  response.headers.set("custom-header","custom-value")
+
+  return response
 
   // Redirecting using conditonal statement
-  if (req.nextUrl.pathname === "/profile") {
+  // if (req.nextUrl.pathname === "/profile") {
 
-    // return NextResponse.redirect(new URL("/hello", req.url));
+  //   // return NextResponse.redirect(new URL("/hello", req.url));
 
-    // Now by .rewrite() the url remain "/profile" but the content of "/hello" will be shown 
-    return NextResponse.rewrite(new URL("/hello", req.url));
+  //   // Now by .rewrite() the url remain "/profile" but the content of "/hello" will be shown
+  //   return NextResponse.rewrite(new URL("/hello", req.url));
 
-  }
+  // }
 
   // Redirecting to home page "/"
   // return NextResponse.redirect(new URL("/", req.url));
 }
 
-// The config object with the matcher property specifies which routes the middleware applies to. In our case, the middleware targets "/profile" route only. 
+// The config object with the matcher property specifies which routes the middleware applies to. In our case, the middleware targets "/profile" route only.
 
-// Commenting it means this middleware will execute for every request made on our application 
+// Commenting it means this middleware will execute for every request made on our application
 
-// export const config = {  
+// export const config = {
 //   matcher: "/profile",
 // };
 
@@ -43,5 +55,7 @@ export function middleware(req: NextRequest) {
  (b). Conditional statements
 
   2. Middleware also allows URL rewrites which is useful for SEO. It involves modifying the incoming request URL before it reaches your application's routing logic. This process allows developers to present clean, user-friendly URLs while internally mapping them to different endpoints or resources.
+
+  3. We can also use middleware to handle both cookies and headers of both request and response object. 
 
 */
