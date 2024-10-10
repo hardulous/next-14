68fdc30783f7a4e60605c0815a4724e758ada10c
hardulous <bishtaman120@gmail.com>
2024-10-10 23:44:58 +0530

    rendering 90% done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,17 @@
+import { cookies } from "next/headers";
+
 // Here metadata for this about page contain same property "title" as present in root layout.tsx so this one will replace the one present in root layout.tsx. 
 export const metadata = {
   title: "About Aman",
 };
 
 const About = () => {
+  
+  // Dynamic function for dynamic rendering
+  const cookieStore = cookies()
+  const theme = cookieStore.get("theme")
+  console.log(theme)
+
   return (
     <div>About</div>
   )
